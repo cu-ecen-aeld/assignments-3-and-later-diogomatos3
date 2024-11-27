6e382e1db603aefb6bce62c80cfb4be76c125dcc
Diogo Matos <diogo.matos3@pt.bosch.com>
2024-11-27 16:17:41 +0000

    Tested locally

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,16 +35,16 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
-    # echo "Building the kernel"
-    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    # make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    # make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
-    # make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    echo "Building the kernel"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper # Cleaning the Kernel Source Tree
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig # Generating Default Configuration
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all # Building the Kernel
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules # Building the Kernel Modules
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs # Building the Kernel Device Tree Blobs
 fi
 
 echo "Adding the Image in outdir"
-# cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -55,11 +55,11 @@ then
 fi
 
 # TODO: Create necessary base directories
-# mkdir ${OUTDIR}/rootfs
-# cd ${OUTDIR}/rootfs
-# mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var 
-# mkdir -p usr/bin usr/lib usr/sbin
-# mkdir -p var/log
+mkdir ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var 
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -68,68 +68,68 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
-    # make distclean
-    # make defconfig
+    make distclean # cleans the BusyBox source tree by removing all generated files, including configuration files, build artifacts, and other temporary files. It ensures that the build starts from a clean state.
+    make defconfig # generates a default configuration file for BusyBox
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
-# make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-# make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+echo "Library dependencies - program interpreter"
+${CROSS_COMPILE}readelf -a busybox | grep "program interpreter"
+echo "Library dependencies - Shared library"
+${CROSS_COMPILE}readelf -a busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-# echo "Adding library dependencies"
-# SYSROOT=$(realpath $(${CROSS_COMPILE}gcc -print-sysroot))
-# ${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter" | while read -r line; do
-#     INTERPRETER=$(echo "$line" | awk '{print $NF}' | sed 's/]$//')
-#     echo "Copying ${INTERPRETER} to ${OUTDIR}/rootfs"
-#     cp -L "${SYSROOT}${INTERPRETER}" "${OUTDIR}/rootfs/lib"
-# done
+echo "Adding library dependencies"
+SYSROOT=$(realpath $(${CROSS_COMPILE}gcc -print-sysroot))
+echo "SYSROOT is ${SYSROOT}"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter" | while read -r line; do
+    INTERPRETER=$(echo "$line" | awk '{print $NF}' | sed 's/]$//')
+    echo "Copying ${INTERPRETER} to ${OUTDIR}/rootfs"
+    cp -L "${SYSROOT}${INTERPRETER}" "${OUTDIR}/rootfs/lib"
+done
 
-# ${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library" | while read -r line; do
-#     LIBRARY=$(echo "$line" | awk '{print $NF}' | sed 's/[][]//g')
-#     echo "Copying ${LIBRARY} to ${OUTDIR}/rootfs"
-#     cp -L "${SYSROOT}/lib64/${LIBRARY}" "${OUTDIR}/rootfs/lib64"
-# done
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library" | while read -r line; do
+    LIBRARY=$(echo "$line" | awk '{print $NF}' | sed 's/[][]//g')
+    echo "Copying ${LIBRARY} to ${OUTDIR}/rootfs"
+    cp -L "${SYSROOT}/lib64/${LIBRARY}" "${OUTDIR}/rootfs/lib64"
+done
 
 # TODO: Make device nodes
-# echo "Creating device nodes"
-# sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
-# sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
+echo "Creating device nodes"
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
-# echo "Building writer application"
-# cd ${FINDER_APP_DIR}
-# make clean
-# make CROSS_COMPILE=${CROSS_COMPILE}
+echo "Building writer application"
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-# echo "Copying Finder scripts and writer utility"
-# cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
-# cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
-# cp -r ${FINDER_APP_DIR}/conf/ ${OUTDIR}/rootfs/home/conf
-# # cp -r ${FINDER_APP_DIR}/conf/username.txt ${OUTDIR}/rootfs/home/conf
-# cp ${FINDER_APP_DIR}/conf/assignment.txt ${OUTDIR}/rootfs/home/
-# cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
-# cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+echo "Copying Finder scripts and writer utility"
+cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
+cp -r ${FINDER_APP_DIR}/conf/ ${OUTDIR}/rootfs/home/conf
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
 
 
 # echo "Modifying finder-test.sh"
-# sed -i 's|\.\./conf/assignment.txt|conf/assignment.txt|g' ${OUTDIR}/rootfs/home/finder-test.sh
+sed -i 's|\.\./conf/assignment.txt|conf/assignment.txt|g' ${OUTDIR}/rootfs/home/finder-test.sh
 
 # TODO: Chown the root directory
-# echo "Changing ownership of root filesystem to root"
-# sudo chown -R root:root ${OUTDIR}/rootfs
+echo "Changing ownership of root filesystem to root"
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
-# echo "Creating initramfs"
-# cd ${OUTDIR}/rootfs
-# find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
-# cd ${OUTDIR}
-# gzip -f ${OUTDIR}/initramfs.cpio
+echo "Creating initramfs"
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f ${OUTDIR}/initramfs.cpio
